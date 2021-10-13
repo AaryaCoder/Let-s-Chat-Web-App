83bc817edb1eb08448c8b9f6ed8625a94f5b2e9b
AaryaCoder <84276676+AaryaCoder@users.noreply.github.com>
2021-10-13 18:09:36 -0400

    Add files via upload

--- Kwitter.js
+++ Kwitter.js
@@ -1,4 +1,8 @@
 function addUser(){
     user_name=document.getElementById("user_name").value;
     firebase.database().ref("/").child(user_name).update({purpose:"adding user"});
+    localStorage.setItem("user_name",user_name);
+    
 }
+
+window.location("Kwitter_room.html");
